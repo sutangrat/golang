6c4b308541696ff4db15b0e03609ed8eafe43b2b
sutangrat <59154721+sutangrat@users.noreply.github.com>
2019-12-24 15:53:32 +0700

    Update 078.go

--- 078.go
+++ 078.go
@@ -11,6 +11,7 @@ type ExampleStruct struct{}
 func (this *ExampleStruct struct) Plus (args []float64,rely *float64) erorr{
 	sum := 0.0
 	for _, v := range args {
-		
+		sum = sum + v
 	}
+	*reply = sum
 }
