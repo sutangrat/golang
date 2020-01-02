cda1285aac50fdf915ac40846e1e86d40b398b4d
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-01-02 15:58:57 +0700

    Update 0.35.go

--- 0.35.go
+++ 0.35.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 	fmt.Print("input : ")
 	var name string
+	var age int
 	var height float32
 	var Weigth float32
 	n, err := fmt.Scan(&name, &age, &Weigth, &height)
 	fmt.Println(name, age, weigth, height)
 	fmt.Println('number of argument ', n)
+	fmt.Println('error ' ,err)
 }
