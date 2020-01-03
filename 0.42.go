7b87bf40d09e1df1bebb4adb9e3391c3b4d8c8b2
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-01-03 22:06:17 +0700

    Update 0.42.go

--- 0.42.go
+++ 0.42.go
@@ -4,5 +4,7 @@ import "fmt"
 
 func main() {
 	student := []string{"Goku", "Gohan", "vegeta", "Piccolo", "Krillin",
-    "Yamcha", "Blum", "Yidel", "Oolong", "Puar"}
+	"Yamcha", "Blum", "Yidel", "Oolong", "Puar"}
+	
+	fmt.Println(student)
 }
