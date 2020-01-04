1ace85e449f19d4c177dc3ce793e753b702aec18
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-01-04 20:44:21 +0700

    Create 0.57.go

---
+++ 0.57.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [5]int{0 ,1, 2, 3, 4}
+	fmt.Println(number[1])
+	numbers[1] = 10
+	fmt.Println(numbers[1])
+	length := len(numbers)
+	fmt.Println("length =",length)
+}
