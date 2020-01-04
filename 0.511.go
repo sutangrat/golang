2def7417ad3922619e79bc0d16e7d943499f10f7
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-01-04 20:50:32 +0700

    Create 0.511.go

---
+++ 0.511.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func main() {
+	alphabets := [4]string{"A", "B", "C", "D"}
+	fmt.Println(alphabets)
+
+    x := alphabets[0:2]
+	fmt.Println(x)
+
+	y := x[2:4]
+	fmt.Println(y)
+	
+    z := y[0:1]
+	fmt.Println(z)
+	
+    z[0] = "x"
+	fmt.Println(alphabets)
+	
+}
