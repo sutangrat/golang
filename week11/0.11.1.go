cd87b7db843f8b30462e6fcef6de5eefefb72805
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:11:46 +0700

    Create 0.11.1.go

---
+++ week11/0.11.1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func say(txt string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(i, ":", txt)
+	}
+}
+
+func main()  {
+	go say("Hello")
+	go say("Hi")
+	var input string
+	fmt.Scanln(&input)
+}	
