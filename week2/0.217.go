76cedb83d68829852d6aa9e446caaee2ace53d39
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:12:51 +0700

    Create 0.217.go

---
+++ week2/0.217.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("10 is of type %T\n",10)
+
+	fmt.Print("10 is of type %T\n",string(10) )
+
+	fmt.Print("10 is of type %T\n",float64(10) )
+	
+} 
