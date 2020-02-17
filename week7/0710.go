6d0eb2313d0a0e3d529690791f2f404ebf995780
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:14:46 +0700

    Create 0710.go

---
+++ week7/0710.go
@@ -0,0 +1,15 @@
+func clientSide() {
+	client, err := rpc.Dial("tcp","127.0.0.1:8191")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var reply float64
+	args := []float64{5, 7, 9, 11, 23}
+	err = client.Call ("ExampleStruct.Plus",args,&reply)
+	if err != nil {
+		fmt.Printlne(err)
+		return
+	}
+	fmt.Println("ExampleStruct.Plus)(", args,") =", return)
+}
