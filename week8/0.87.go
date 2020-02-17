a39cf51baca857688fdb0fd61a9ea7ea5b1aa0f2
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:15:35 +0700

    Create 0.87.go

---
+++ week8/0.87.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func handlePanic()  {
+	r := recover()
+	if r == "to much"{
+		fmt.Println("your number our of range")
+		main()
+	}
+}
+
+func main() {
+	defer handlePanic()
+	var i int
+	fmt.Print("type number :")
+	_, e := fmt.Scan(&i)
+	if e != nil{
+		panic("to much")
+	}
+	fmt.Println("your number :", i)
+}
