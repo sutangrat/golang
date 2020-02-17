cf5f7b560cdbea928176902c2f924515d00b678a
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:11:48 +0700

    Create 0.11.2.go

---
+++ week11/0.11.2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(txt string)  {
+	for i := 0, i < 3; i++ {
+		fmt.Println(time.Now(),":", i, ":",txt)
+		time.Sleep(time.Millisecond)
+	}
+}
+func main()  {
+	go say("Hello")
+	go say("Hi")
+	var input string
+	fmt.Scanln(&input)
+}	
