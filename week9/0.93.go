52a926ea5fa6e80c923853fc140d96e02ccbccef
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:15:48 +0700

    Create 0.93.go

---
+++ week9/0.93.go
@@ -0,0 +1,18 @@
+package main
+
+import  "fmt"
+
+type student struct {
+	name string
+	age int
+	email string
+}
+
+func (std student) introduce() {
+	fmt.Println("Hello my name is ", std.name)
+}
+
+func main () {
+	std := student{name: "Goku"}
+	std.introduce()
+}
