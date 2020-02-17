fd0391369b4b627157c6afd58f8f5bedd54996eb
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:15:10 +0700

    Create 078.go

---
+++ week7/078.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+)
+
+type ExampleStruct struct{}
+
+func (this *ExampleStruct struct) Plus (args []float64,rely *float64) erorr{
+	sum := 0.0
+	for _, v := range args {
+		sum = sum + v
+	}
+	*reply = sum
+	return  nil
+}
