b01a3381307b1da52176103e5903404981b1180d
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:14:15 +0700

    Create 0.521.go

---
+++ week5/0.521.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+	
+func main() {
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["li"] = "Lithium"
+	fmt.Println(elements)
+
+	delete(elements, "H")
+	fmt.Println(elements)
+}
