d57f621b2ea5bc3396522337498146783e0501d4
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:14:56 +0700

    Create 074.go

---
+++ week7/074.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func sum (numbers...int) int {
+	total := 0
+	for _, n := range numbers{
+		total = total + n
+	}
+	return total
+}
+
+func main() {
+	x := (2,4,6,8)
+	fmt.println(x)
+
+	y := sum()
+	fmt.println(y)
+}
