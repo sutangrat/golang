ce113398ac9c9085f8907b4593e604321b2fdbb9
sutangrat <59154721+sutangrat@users.noreply.github.com>
2020-02-17 09:14:10 +0700

    Create 0.52.go

---
+++ week5/0.52.go
@@ -0,0 +1,10 @@
+package  main
+
+import "fmt"
+
+func main() {
+	student := []string{"Goku", "Gohan", "vegeta", "Piccolo", "Krillin",
+	"Yamcha", "Blum", "Yidel", "Oolong", "Puar"}
+	
+	fmt.Println(student)
+}
